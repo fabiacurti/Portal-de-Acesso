204cc4225fccfdbd9ee04020f6170322f63d7c8f
Fabia Curti De Paula <ffabia@latam.stefanini.com>
2023-02-14 11:24:39 -0300

    First Commit

---
+++ Data/Models/Acesso/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Models.Acesso
+{
+    public class Login
+    {
+        public Guid Id { get; set; }
+        public string Usuario { get; set; }
+        public Guid UsuarioCorregedoria { get; set; }
+        public string Senha { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+
+        public Guid IdUsuario { get; set; }
+        public bool Bloqueado { get; set; }
+
+
+    }
+}
---
+++ Data/Models/Acesso/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Models.Acesso
+{
+    public class Usuario
+    {
+        public string Id { get; set; }
+        public Corregedoria  IdCorregedoria  { get; set; }
+
+
+    }
+}
